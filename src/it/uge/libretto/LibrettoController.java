12bae5129e6bfa02f960aaafe4d05a3d04dbcfec
eugenio <eugenio@pc-eugenio>
2017-06-11 00:56:51 +0200

    versione iniziale

---
+++ src/it/uge/libretto/LibrettoController.java
@@ -0,0 +1,5 @@
+package it.uge.libretto;
+
+public class LibrettoController {
+	
+}
---
+++ src/it/uge/libretto/Main.java
@@ -0,0 +1,31 @@
+package it.uge.libretto;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+/**
+ * prova libretto giugno 2017
+ * @author eugenio
+ *
+ */
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Libretto.fxml"));
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
